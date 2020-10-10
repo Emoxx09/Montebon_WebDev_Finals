7ea4340967435156d864eda6b645fbcd880a8510
KMontebon <70185597+KMontebon@users.noreply.github.com>
2020-10-11 00:06:52 +0800

    Create fetch.js

---
+++ exam/js/fetch.js
@@ -0,0 +1,27 @@
+function home()
+{
+     location.href = "home.html";
+}
+
+function profile()
+{
+     location.href = "profile.html";
+} 
+
+function cart()
+{
+     location.href = "cart.html";
+} 
+
+function contactUs()
+{
+     location.href = "contactus.html";
+} 
+
+function login(){
+	location.href = "loginsignup.html";
+}
+
+function portfolio(){
+	window.location.href = "https://kmontebon.github.io/Montebon_WebDev_MidTerm/exam/part2b/montebon_portfolio.html";
+}
